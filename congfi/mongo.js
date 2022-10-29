c7a6d4c20dd1aa730f131bd7008bb0d1a84f528e
sahiljoya <sahildeshwali1212@gmail.com>
2022-10-29 10:12:32 +0530

    prodect last--all

--- congfi/mongo.js
+++ congfi/mongo.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const connectdb = async() => {
-  const conect = await mongoose.connect("mongodb+srv://prod:kRLBu5y1QFnt6D3g@cluster0.wwmlxzf.mongodb.net/?retryWrites=true&w=majority/amazon",
+  const conect = await mongoose.connect("mongodb+srv://prod:kRLBu5y1QFnt6D3g@cluster0.wwmlxzf.mongodb.net/?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -4,13 +4,15 @@ import connectDB from "./congfi/mongo.js";
 import atrouter from "./router/cetegory.router.js";
 import subRouter from "./router/sub.router.js";
 import imagerouter from "./router/prodect.image.router.js";
+import { config } from "dotenv";
 const app = Express()
 app.use(Express.json())
 connectDB()
+config()
 app.use(imagerouter)
 app.use(subRouter)
 app.use(atrouter)
 app.use(router)
-app.listen(3003, (req, res) => {
+app.listen(process.env.PORT ||  3003, (req, res) => {
     console.log("server port:3003");
 })
