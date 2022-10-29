f274ce018b662ba10e0080c3ed765c8068fd44fe
sahiljoya <sahildeshwali1212@gmail.com>
2022-10-29 14:56:42 +0530

    problum solve

--- contoller/prodect.controller.js
+++ contoller/prodect.controller.js
@@ -3,36 +3,35 @@ import prodect from "../models/prodect.model.js"
 export const im = async (req, res) => {
     try {
         var imageallData = []
-       req.files.forEach(image => {
-        var imagetype = ''
-        if (image.mimetype == 'png') {
-            imagetype = 'png'
-        }else if(image.mimetype == 'jpg'){
-            imagetype = 'jpg'
-        }
-        let imageData = {
-            type:imagetype,
-            path:image.filename,
-            fullpath:"https://hello-api-prodect.herokuapp.com"+image.path
-        }
-        console.log(imageData);
-        imageallData.push(imageData)
-       });
-       req.body.image = imageallData
-       const createData = await prodect.create(req.body)
-       if (createData) {
+        req.files.forEach(image => {
+            var imagetype = ''
+            if (image.mimetype == 'image/png') {
+                imagetype = 'png'
+            } else if (image.mimetype == 'image/jpg') {
+                imagetype = 'jpg'
+            }
+            let imageData = {
+                type: imagetype,
+                path: image.filename,
+                fullpath: "https://hello-api-prodect.herokuapp.com" + image.path
+            }
+            imageallData.push(imageData)
+        });
+        req.body.image = imageallData
+        const createData = await prodect.create(req.body)
+        if (createData) {
             res.send({
-                status:true,
-                msg:"prodect created success",
-                data:createData
+                status: true,
+                msg: "prodect created success",
+                data: createData
             })
-       }else{
-        res.send({
-            status:false,
-            msg:'data dot sai h sai karo',
-            data:{}
-        })
-       }
+        } else {
+            res.send({
+                status: false,
+                msg: 'data dot sai h sai karo',
+                data: {}
+            })
+        }
     } catch (error) {
         res.send(error)
     }
